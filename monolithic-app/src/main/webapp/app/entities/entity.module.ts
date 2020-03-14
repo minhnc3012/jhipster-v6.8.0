493104043c241528bc1db17dbc8bde23c1786a23
minhnguyenformos <minh.nguyen@formos.com>
2020-03-14 13:31:22 +0700

    import file jhipster-jdl

--- monolithic-app/src/main/webapp/app/entities/entity.module.ts
+++ monolithic-app/src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,38 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'region',
+        loadChildren: () => import('./region/region.module').then(m => m.MonolithicappRegionModule)
+      },
+      {
+        path: 'country',
+        loadChildren: () => import('./country/country.module').then(m => m.MonolithicappCountryModule)
+      },
+      {
+        path: 'location',
+        loadChildren: () => import('./location/location.module').then(m => m.MonolithicappLocationModule)
+      },
+      {
+        path: 'department',
+        loadChildren: () => import('./department/department.module').then(m => m.MonolithicappDepartmentModule)
+      },
+      {
+        path: 'task',
+        loadChildren: () => import('./task/task.module').then(m => m.MonolithicappTaskModule)
+      },
+      {
+        path: 'employee',
+        loadChildren: () => import('./employee/employee.module').then(m => m.MonolithicappEmployeeModule)
+      },
+      {
+        path: 'job',
+        loadChildren: () => import('./job/job.module').then(m => m.MonolithicappJobModule)
+      },
+      {
+        path: 'job-history',
+        loadChildren: () => import('./job-history/job-history.module').then(m => m.MonolithicappJobHistoryModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
